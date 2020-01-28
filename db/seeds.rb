192a7cfffcdb962303d54d9c3408c8b4a943e96a
Jamie Reynolds <jamie.s.a.reynolds@gmail.com>
2020-01-28 19:55:04 +0000

    Ready for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Deleting cocktails"
+Cocktail.destroy_all
 
 puts "Deleting ingredients"
 Ingredient.destroy_all
